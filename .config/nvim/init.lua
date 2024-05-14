51c82f30052e0f6be7c5b0d3b865c3ae9aaa5b6d
Michael Mark <michael.mark@oit.edu>
2024-05-14 14:57:05 -0700

    Removed large amount of comments

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -82,7 +82,6 @@ require('lspconfig').lua_ls.setup{
 -- Nvim-tree config
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
--- vim.opt.termguicolors = true
 require("nvim-tree").setup()
 
 -- Lualine config
@@ -129,35 +128,12 @@ lualine.setup {
 
 lualine.refresh()
 
--- Cmp config
-
--- import nvim-cmp plugin safely
--- local cmp_status, cmp = pcall(require, "cmp")
--- if not cmp_status then
---   return
--- end
-
--- import luasnip plugin safely
--- local luasnip_status, luasnip = pcall(require, "luasnip")
--- if not luasnip_status then
---   return
--- end
-
 local cmp = require('cmp')
 cmp.setup({
     snippet = {
-      -- REQUIRED - you must specify a snippet engine
       expand = function(args)
-        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-        -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+        require('luasnip').lsp_expand(args.body)
       end,
-    },
-    window = {
-      -- completion = cmp.config.window.bordered(),
-      -- documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -168,25 +144,11 @@ cmp.setup({
     }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
-      -- { name = 'vsnip' }, -- For vsnip users.
-      { name = 'luasnip' }, -- For luasnip users.
-      -- { name = 'ultisnips' }, -- For ultisnips users.
-      -- { name = 'snippy' }, -- For snippy users.
+      { name = 'luasnip' },
     }, {
       { name = 'buffer' },
     })
   })
-
-  -- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
-  -- Set configuration for specific filetype.
-  --[[ cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-      { name = 'git' },
-    }, {
-      { name = 'buffer' },
-    })
- })
- require("cmp_git").setup() ]]-- 
 
   -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
   cmp.setup.cmdline({ '/', '?' }, {
@@ -206,13 +168,6 @@ cmp.setup({
     }),
     matching = { disallow_symbol_nonprefix_matching = false }
   })
-
-  -- -- Set up lspconfig.
-  -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
-  -- -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-  -- require('lspconfig')['clangd'].setup {
-  --   capabilities = capabilities
-  -- }
 
 -- My binds
 vim.g.mapleader = ' '
@@ -240,8 +195,6 @@ end, {expr = true, remap = false})
 vim.keymap.set('i', '<C-right>', function()
     vim.fn['copilot#Accept']("")
     local bar = vim.fn['copilot#TextQueuedForInsertion']()
-    -- Dear Copilot: How do I make this line trigger split on (, ), <, and >?
-    --return vim.fn.split(bar,  [[\s\(\)\[\]<>]])[1]
     return vim.fn.split(bar,  [[[ .\(\)\<\>]\zs]])[1]
 end, {expr = true, remap = false})
 
